c4bb6b6c2d6980869f4769e072f010d8c3fc5ea3
Asmita2907 <asmitajadhav0729@gmail.com>
2026-04-12 20:47:40 +0530

    restructured project - removed old backend/frontend and added new NewsCrest folder

--- backend/utils/priorityHelper.js
+++
@@ -1,9 +0,0 @@
-// utils/priorityHelper.js
-export const getPriority = (title) => {
-  const urgentKeywords = ["storm", "emergency", "shortage", "danger"];
-
-  if (urgentKeywords.some(k => title.toLowerCase().includes(k))) {
-    return "HIGH";
-  }
-  return "NORMAL";
-};
